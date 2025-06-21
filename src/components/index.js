9ca56b03ccbb9f72225d4af16ae34d5068eff1e4
NorthBlueSrgn <137292638+NorthBlueSrgn@users.noreply.github.com>
2025-06-21 18:23:39 +0100

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// src/components/index.js
+export { TaskItem } from './TaskItem';
+export { PathCard } from './PathCard';
+export { NewPathModal } from './NewPathModal';
+export { CustomPathModal } from './CustomPathModal';
+export { Zero2OneApp } from './Zero2OneApp';
