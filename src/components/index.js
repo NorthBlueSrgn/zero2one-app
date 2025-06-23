c081ed1740b2f9724b730b77440a90c03233c84b
NorthBlueSrgn <137292638+NorthBlueSrgn@users.noreply.github.com>
2025-06-23 11:44:51 +0100

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
 // src/components/index.js
-export { Zero2OneApp } from './Zero2OneApp';
 export { CustomPathModal } from './CustomPathModal';
 export { NewPathModal } from './NewPathModal';
 export { TaskItem } from './TaskItem';
-export { PathCard } from './PathCard';
